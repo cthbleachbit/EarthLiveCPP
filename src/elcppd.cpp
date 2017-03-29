261ddf4db081cd88d8284def4b8ff4437f2ba367
James Chai <cth451@gmail.com>
2017-03-28 23:22:18 -0500

    daemon skeleton.

--- src/elcppd.cpp
+++ src/elcppd.cpp
@@ -16,23 +16,40 @@
  * ==========================================================
 **/
 
-#include "lib/io.hpp"
+#include <string>
 #include <iostream>
+#include <fstream>
+#include <curl/curl.h>
+#include "lib/io.hpp"
+
+const std::string SITE = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/";
+const std::string DAEMON_LOG = "/.earthlivecpp/daemon.log";
+
+static size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream)
+{
+	size_t written = fwrite(ptr, size, nmemb, (FILE *)stream);
+	return written;
+}
 
 void printHelp() {
-	std::cerr << "Usage: elcpp-daemon <interval> <density> <debug>" << std::endl;
+	std::cerr << "Usage: elcppd <interval> <density> <debug>" << std::endl;
 	std::cerr << "\tinterval:    refresh interval in minutes." << std::endl;
 	std::cerr << "\tdensity:     numerical value for image tiles." << std::endl;
 	std::cerr << "\tdebug:       1 for debugging logs in log file" << std::endl;
 }
 
-void refreshRoutine() {
-
+void downloadRoutine(std::string& url, std::ofstream& file) {
+	CURL *curl;
+	CURLcode res;
 }
 
 int main(int argc, char *argv[]) {
-	if (argc != 3) {
-		std::cerr << "Need 3 arguments." << std::endl;
+	// setting up logging
+	
+	// check update
+	curl_global_init(CURL_GLOBAL_DEFAULT);
+	if (argc != 4) {
+		std::cerr << "Need 3 arguments. Got " << argc - 1 << "." << std::endl;
 		return 1;
 	}
 }
---
+++ src/lib/defines.hpp
@@ -0,0 +1,17 @@
+/**
+ * ==========================================================
+ * EarthLiveCPP
+ * constants definition header
+ * 
+ * Copyright (C) Tianhao Chai (cth451) <cth451@gmail.com>
+ * 
+ * Inspired by bitdust's [ https://github.com/bitdust ]
+ * Licensed under LGPL 3
+ * 
+ * This program is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE. See the GNU Lesser General Public
+ * License for more details.
+ * ==========================================================
+**/
