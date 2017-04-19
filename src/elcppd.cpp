aea49551bb75a1a0a463e706d924fc10b335eb91
James Chai <cth451@gmail.com>
2017-04-19 10:46:42 -0500

    tried to make a better skeleton

--- src/elcppd.cpp
+++ src/elcppd.cpp
@@ -22,16 +22,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <boost/filesystem.hpp>
+#include <boost/format.hpp>
 #include <curl/curl.h>
 
 #include "lib/io.hpp"
 
 const std::string SITE = "http://himawari8-dl.nict.go.jp/himawari8/img/D531106/";
 
+const std::string& assembleUrl(int x, int, y, int dimens, int datetime) {
+	if (d == 0) {
+		return SITE.append("latest.json");
+	} else {
+		std::string urlWithDimens = SITE.append(std::to_string(d) + "d/550/");
+		std::string tStr = std::to_string(datetime);
+		// structure yyyy/mm/dd/hhmmss
+		std::string finalUrl = urlWithDimens + tStr.substr(0,4) + "/" + tStr.substr(4,6) + "/" + tStr.substr(6,8) + "/" + tStr.substr(8);
+		return finalUrl;
+	}
+}
+
 static size_t write_data(void *contents, size_t size, size_t nmemb, void *stream)
 {
-	size_t written = fwrite(contents, size, nmemb, (FILE *)stream);
-	return written;
+	((std::string*)stream)->append((char*)contents, size * nmemb);
+	return size * nmemb;
 }
 
 // regular help usage
@@ -43,34 +56,31 @@ void printHelp() {
 }
 
 // The function that actually downloads, returns a CURLcode. Not 0 = fail
-int downloadRoutine(std::string& url, std::string& file) {
-	CURL *curl;
-	FILE *out;
+int downloadRoutine(std::string& url, std::string& blob) {
+	CURL* curl;
 	CURLcode retCode;
-	const char *c_url = url.c_str();
-	const char *c_fname = file.c_str();
+	const char* c_url = url.c_str();
 	curl = curl_easy_init();
 	if (curl) {// if successfully created curl struct
-		out = fopen(c_fname, "wb"); // open with write + binary
 		curl_easy_setopt(curl, CURLOPT_URL, c_url);
 		curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
-		curl_easy_setopt(curl, CURLOPT_WRITEDATA, out);
+		curl_easy_setopt(curl, CURLOPT_WRITEDATA, imageBlob);
 		retCode = curl_easy_perform(curl);
 		curl_easy_cleanup(curl);
-		fclose(out);
 	}
 	return retCode;
 }
+
 
 int main(int argc, char *argv[]) {
 	// setting up logging and storage space
 	const std::string HOME_DIR = getenv("HOME");
 	const std::string CONFIG_DIR = HOME_DIR + "/.earthlivecpp/";
-	boost::filesystem::path confDir(CONFIG_DIR);
+	const boost::filesystem::path confDir(CONFIG_DIR);
 	if (!boost::filesystem::exists(confDir)) {
 		boost::filesystem::create_directory(confDir);
 	}
-	//ioInstance logHandler(CONFIG_DIR + "log.txt");
+	// ioInstance logHandler(CONFIG_DIR + "log.txt");
 
 	// network initialize
 	curl_global_init(CURL_GLOBAL_DEFAULT);
